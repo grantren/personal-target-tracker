27c68fcc0367d5ae95631bb788d4cc3bcb5d3b63
grant <grant@grant-VirtualBox.(none)>
2012-12-19 04:24:28 +0800

    first commit

---
+++ application/controllers/app.php
@@ -0,0 +1,12 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class App extends CI_Controller {
+
+	public function index()
+	{
+		echo "This is a test message!";
+	}
+}
+
+/* End of file app.php */
+/* Location: ./application/controllers/app.php */
